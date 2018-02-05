773690e8b3396ebbb1b94e31ed60434b293c8111
Rosanne <zhenshan.luo@outlook.com>
2018-02-05 21:43:13 +0800

    add filter

--- tencentJob.py
+++ tencentJob.py
@@ -1,9 +1,15 @@
 from urllib import request
 from bs4 import BeautifulSoup
 import csv
+import re
 #深圳，技术类岗位
-url='https://hr.tencent.com/position.php?lid=2218&tid=87'
-dataFile="tencentJob.csv"
+url='https://hr.tencent.com/position.php?lid=2218&tid=87&lid=2218&start='
+dataFile="/Users/Rosanne/Desktop/tencentJob.csv"
+
+#白名单记录我们需要查找的职位的关键词
+#黑名单J留我们不需要查找的职位的关键词
+whiteFilter='安全|研发|开发'
+blackFilter='游戏|视觉|自然语言'
 
 def getDetailInfo(url):
 	html=request.urlopen(url)
@@ -12,8 +18,8 @@ def getDetailInfo(url):
 	
 	trList=table.find_all('tr')
 
+#获取职位名称,工作地点，招聘人数，工作职责，工作要求
 	title=trList[0].find('td').string
-	print(title)
 	
 	info2=trList[1].find_all('td')
 	address=info2[0].text.split('：')[1]
@@ -23,14 +29,23 @@ def getDetailInfo(url):
 	dutys=trList[2].find('ul').find_all(text=True)
 	dutyStr=''
 	for duty in dutys:
-		dutyStr=dutyStr+duty
+		dutyStr=dutyStr+'\n'+duty
 	print(dutyStr)
 	demands=trList[3].find('ul').find_all(text=True)
 	demandStr=''
 	for demand in demands:
-		demandStr=demandStr+demand
+		demandStr=demandStr+'\n'+demand
 	print(demandStr)
 	return [title, address, number, dutyStr, demandStr]
+
+#对职位进行过滤，返回值为True表示我们需要进一步获取相关信息
+def filterJob(job):
+	w=re.match(whiteFilter,job)
+	b=re.match(blackFilter,job)
+	if (w and (not b)):
+		return True
+	else
+		return False
 
 def getJob(url):
 	html=request.urlopen(url)
@@ -39,16 +54,34 @@ def getJob(url):
 	trs=table.find_all('tr')
 	trNum=len(trs)
 
-#tr=trs[1]
 	for tr in trs[1:trNum-1]:
 		urlJob=tr.find('a')['href']
-#print(type(urlJob))
+		job=urlJob.find('a').text
+		if not filterJob(job):
+				continue
+		print("[*] get job: %s" % job)
 		urlJob='https://hr.tencent.com/'+urlJob
-		data=getDetailInfo(urlJob)
-		csvWriter.writerow((data[0],data[1],data[2],data[3],data[4]))
+		releaseData=tr.find_all('td')[4].string
+		print(urlJob)
+		try:
+			data=getDetailInfo(urlJob)
+			csvWriter.writerow((data[0],releaseData,data[1],data[2],data[3],data[4]))
+		except Exception as e:
+			pass
 
-#print(trs.prettify())
 csvFile=open(dataFile,'w')
 csvWriter=csv.writer(csvFile, delimiter=',')
-csvWriter.writerow(('职位','工作地点','招聘人数','工作职责','工作要求'))
-getJob(url)
+csvWriter.writerow(('职位','发布时间','工作地点','招聘人数','工作职责','工作要求'))
+
+i=0
+while True:
+	urlPage=url+str(i*10)+'#a'
+	html=request.urlopen(urlPage)
+	if not html:
+		break
+	print('[*] process page %d' % i+1)
+	getJob(url)
+	i=i+1
+
+print('[*]finished')
+#getJob(url)
